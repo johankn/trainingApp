ca8a04bcce35b2fe1da5de26892e73945b77c43d
johakn <johakn@stud.ntnu.no>
2023-02-15 08:48:08 +0100

    added sign in authentication to firebase
    to do: make registration form so we can actually register new user to the database

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,24 +1,51 @@
+import React, { useState } from "react";
+import { auth } from "../firebase-config";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import '../resources/login.css';
 import Navbar from '../components/Navbar';
 
-function LoginBody(){
-  return(
-    <form>
-      <label> Username </label>
-      <input></input>
-      <label> Password </label>
-      <input></input>
-      <input type="submit" value="Login"></input>
-    </form>
-  );
-}
 
-function Login() {
-  return (
-    <div className="login">
+
+function Login({ setIsAuth }) {
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+
+  const onLogin = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        //setIsAuth(true);
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
+
+  return(
+
+    <form>  
       <Navbar/>
-      <LoginBody/>
-    </div>
+      <label> Email </label>
+      <input            type="email"
+            id="email"
+            className="form__input"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"></input>
+      <label> Password </label>
+      <input            className="form__input"
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"></input>
+      <input onClick={onLogin} type="submit" value="Login"></input>
+    </form>
   );
 }
 
