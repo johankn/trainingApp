08cc9b605fc656f880559f18b788a91993aaa9b8
andreasandberg <andressa@stud.ntnu.no>
2023-02-17 11:40:28 +0100

    Added days for TrainingProgram
    Co-Authored by: Andreas Ander Li <andrea10@stud.ntnu.no>

--- src/components/TrainingDay.js
+++ src/components/TrainingDay.js
@@ -1,7 +1,17 @@
 import React, { useState } from 'react';
 
-function TrainingDay() {
-  const [exercises, setExercises] = useState([]);
+function TrainingProgram() {
+  const [trainingDays, setTrainingDays] = useState([
+    { name: 'Monday', exercises: [] },
+    { name: 'Tuesday', exercises: [] },
+    { name: 'Wednesday', exercises: [] },
+    { name: 'Thursday', exercises: [] },
+    { name: 'Friday', exercises: [] },
+    { name: 'Saturday', exercises: [] },
+    { name: 'Sunday', exercises: [] },
+  ]);
+
+  const [currentDayIndex, setCurrentDayIndex] = useState(0);
 
   const [newExercise, setNewExercise] = useState({
     name: '',
@@ -16,22 +26,43 @@ function TrainingDay() {
 
   const handleSubmit = event => {
     event.preventDefault();
-    const id = exercises.length + 1;
+    const id = trainingDays[currentDayIndex].exercises.length + 1;
     const exercise = { id, ...newExercise };
-    setExercises([...exercises, exercise]);
+    const updatedTrainingDays = [...trainingDays];
+    updatedTrainingDays[currentDayIndex].exercises.push(exercise);
+    setTrainingDays(updatedTrainingDays);
     setNewExercise({ name: '', sets: '', reps: '' });
   };
 
-  const exerciseList = exercises.map(exercise => (
+  const exerciseList = trainingDays[currentDayIndex].exercises.map(exercise => (
     <div key={exercise.id}>
       <h3>{exercise.name}</h3>
       <p>{`Sets: ${exercise.sets} Reps: ${exercise.reps}`}</p>
     </div>
   ));
 
+  const handleNextDay = () => {
+    const nextDayIndex = currentDayIndex + 1;
+    if (nextDayIndex < trainingDays.length) {
+      setCurrentDayIndex(nextDayIndex);
+    }
+  };
+
+  const handlePreviousDay = () => {
+    const previousDayIndex = currentDayIndex - 1;
+    if (previousDayIndex >= 0) {
+      setCurrentDayIndex(previousDayIndex);
+    }
+  };
+
+  const handleSelectDay = dayIndex => {
+    setCurrentDayIndex(dayIndex);
+  };
+
   return (
     <div>
       <h2>Training Program</h2>
+      <h3>{trainingDays[currentDayIndex].name}</h3>
       <form onSubmit={handleSubmit}>
         <label>
           Exercise Name:
@@ -66,8 +97,15 @@ function TrainingDay() {
         <button type="submit">Add Exercise</button>
       </form>
       {exerciseList}
+      <button onClick={handlePreviousDay}>Previous Day</button>
+      {trainingDays.map((day, index) => (
+        <button key={index} onClick={() => handleSelectDay(index)}>
+          {day.name}
+        </button>
+      ))}
+      <button onClick={handleNextDay}>Next Day</button>
     </div>
   );
-};
+}
 
-export default TrainingDay;
+export default TrainingProgram;
