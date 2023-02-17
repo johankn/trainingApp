3e08b2005b8c2be019745ef25a82609d29e57ac2
johakn <johakn@stud.ntnu.no>
2023-02-17 12:26:55 +0100

    added css file for trainingDay
    co-authored-by <martimae@stud.ntnu.no>

--- src/components/TrainingDay.js
+++ src/components/TrainingDay.js
@@ -1,45 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import "../resources/trainingDay.css";
 
 function TrainingProgram() {
   const [trainingDays, setTrainingDays] = useState([
-    { name: 'Monday', exercises: [] },
-    { name: 'Tuesday', exercises: [] },
-    { name: 'Wednesday', exercises: [] },
-    { name: 'Thursday', exercises: [] },
-    { name: 'Friday', exercises: [] },
-    { name: 'Saturday', exercises: [] },
-    { name: 'Sunday', exercises: [] },
+    { name: "Monday", exercises: [] },
+    { name: "Tuesday", exercises: [] },
+    { name: "Wednesday", exercises: [] },
+    { name: "Thursday", exercises: [] },
+    { name: "Friday", exercises: [] },
+    { name: "Saturday", exercises: [] },
+    { name: "Sunday", exercises: [] },
   ]);
 
   const [currentDayIndex, setCurrentDayIndex] = useState(0);
 
   const [newExercise, setNewExercise] = useState({
-    name: '',
-    sets: '',
-    reps: '',
+    name: "",
+    sets: "",
+    reps: "",
   });
 
-  const handleInputChange = event => {
+  const handleInputChange = (event) => {
     const { name, value } = event.target;
     setNewExercise({ ...newExercise, [name]: value });
   };
 
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     const id = trainingDays[currentDayIndex].exercises.length + 1;
     const exercise = { id, ...newExercise };
     const updatedTrainingDays = [...trainingDays];
     updatedTrainingDays[currentDayIndex].exercises.push(exercise);
     setTrainingDays(updatedTrainingDays);
-    setNewExercise({ name: '', sets: '', reps: '' });
+    setNewExercise({ name: "", sets: "", reps: "" });
   };
 
-  const exerciseList = trainingDays[currentDayIndex].exercises.map(exercise => (
-    <div key={exercise.id}>
-      <h3>{exercise.name}</h3>
-      <p>{`Sets: ${exercise.sets} Reps: ${exercise.reps}`}</p>
-    </div>
-  ));
+  const exerciseList = trainingDays[currentDayIndex].exercises.map(
+    (exercise) => (
+      <div key={exercise.id}>
+        <h3>{exercise.name}</h3>
+        <p>{`Sets: ${exercise.sets} Reps: ${exercise.reps}`}</p>
+      </div>
+    )
+  );
 
   const handleNextDay = () => {
     const nextDayIndex = currentDayIndex + 1;
@@ -55,55 +58,61 @@ function TrainingProgram() {
     }
   };
 
-  const handleSelectDay = dayIndex => {
+  const handleSelectDay = (dayIndex) => {
     setCurrentDayIndex(dayIndex);
   };
 
   return (
-    <div>
-      <h2>Training Program</h2>
-      <h3>{trainingDays[currentDayIndex].name}</h3>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Exercise Name:
-          <input
-            type="text"
-            name="name"
-            value={newExercise.name}
-            onChange={handleInputChange}
-          />
-        </label>
-        <br />
-        <label>
-          Sets:
-          <input
-            type="number"
-            name="sets"
-            value={newExercise.sets}
-            onChange={handleInputChange}
-          />
-        </label>
-        <br />
-        <label>
-          Reps:
-          <input
-            type="number"
-            name="reps"
-            value={newExercise.reps}
-            onChange={handleInputChange}
-          />
-        </label>
-        <br />
-        <button type="submit">Add Exercise</button>
-      </form>
-      {exerciseList}
-      <button onClick={handlePreviousDay}>Previous Day</button>
-      {trainingDays.map((day, index) => (
-        <button key={index} onClick={() => handleSelectDay(index)}>
-          {day.name}
-        </button>
-      ))}
-      <button onClick={handleNextDay}>Next Day</button>
+    <div className="App">
+      <div className="center">
+        <h2>Training Program</h2>
+        <h3>{trainingDays[currentDayIndex].name}</h3>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Exercise Name:
+            <input
+              type="text"
+              name="name"
+              value={newExercise.name}
+              onChange={handleInputChange}
+            />
+          </label>
+          <br />
+          <label>
+            Sets:
+            <input
+              type="number"
+              name="sets"
+              value={newExercise.sets}
+              onChange={handleInputChange}
+            />
+          </label>
+          <br />
+          <label>
+            Reps:
+            <input
+              type="number"
+              name="reps"
+              value={newExercise.reps}
+              onChange={handleInputChange}
+            />
+          </label>
+          <br />
+          <button type="submit">Add Exercise</button>
+        </form>
+        <div className="left">{exerciseList}</div>
+        <div className="day-selector">
+          {trainingDays.map((day, index) => (
+            <button
+              key={index}
+              onClick={() => handleSelectDay(index)}
+              className={index === currentDayIndex ? "active" : ""}
+            >
+              {day.name}
+            </button>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
