f64c84b83ee0ccd66381e8ca047004ff1a3c79e6
Andreas Li <andrea10@stud.ntnu.no>
2023-03-06 14:40:20 +0100

    Added barChart and styled progresstionCharts.js

--- src/pages/ProgressionChart.js
+++ src/pages/ProgressionChart.js
@@ -1,26 +1,43 @@
 import "../resources/progressionChart.css";
-import {LineChart,Line,XAxis,YAxis,CartesianGrid,Tooltip,Legend} from "recharts";
+import {BarChart, Bar,Cell, LineChart,Line,XAxis,YAxis,CartesianGrid,Tooltip,Legend} from "recharts";
+// import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
   const progressionData = [
-    { name: 'Week 1', weight: 20, reps:10 },
-    { name: 'Week 2', weight: 40, reps:20},
-    { name: 'Week 3', weight: 60, reps:10},
-    { name: 'Week 4', weight: 80, reps:20},
-    { name: 'Week 5', weight: 100,reps:30 },
+    { name: 'Week 1', weight: 20, reps: 10,daysTrained: 4},
+    { name: 'Week 2', weight: 40, reps: 20,daysTrained: 2},
+    { name: 'Week 3', weight: 60, reps: 10,daysTrained: 5},
+    { name: 'Week 4', weight: 80, reps: 20,daysTrained: 4},
+    { name: 'Week 5', weight: 100,reps: 30,daysTrained: 7},
   ];
   
 function ProgressionChart(){
     return(
-        <LineChart width={1000} height={500} data={progressionData}>
+      <div className="progressionChartMainPage">
+        <div className="lineChart">
+          <div className="chartTitle">Your Progression</div>
+          <LineChart width={1000} height={500} data={progressionData}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="weight" stroke="#8884d8" activeDot={{ r: 8 }}/>
+              <Line type="monotone" dataKey="reps" stroke="#82ca9d" activeDot={{ r: 8 }}/>
+          </LineChart>
+        </div>
+        <div className="barChart">
+          <div className="chartTitle">Workout's per week</div>
+          <BarChart width={1000} height={500} data={progressionData}>
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey="name" />
             <YAxis />
             <Tooltip />
             <Legend />
-            <Line type="monotone" dataKey="weight" stroke="#8884d8" activeDot={{ r: 8 }}/>
-            <Line type="monotone" dataKey="reps" stroke="#82ca9d" activeDot={{ r: 8 }}/>
-        </LineChart>
-    )
+            <Bar dataKey="daysTrained" fill="#82ca9d"/>
+          </BarChart>
+        </div>
+      </div>
+  )
 }
 
 export default ProgressionChart
