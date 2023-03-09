4a304898da1565ce71074e5d30555e5be5a6f196
Filip Kristoff Ørn <filipko@stud.ntnu.no>
2023-03-09 10:36:07 +0100

    now viewprogram is pretty

--- src/pages/ViewPrograms.js
+++ src/pages/ViewPrograms.js
@@ -13,7 +13,7 @@ function ViewPrograms({ isAuth }) {
   };
 
   const [trainingPrograms, setTrainingPrograms] = useState([]);
-  const [userPrograms, setUserPrograms] = useState([]);
+  const [userPrograms, setUserPrograms] = useState();
   const trainingProgramsCollectionRef = collection(db, "trainingPrograms");
 
   const getPrograms = async () => {
@@ -42,32 +42,120 @@ function ViewPrograms({ isAuth }) {
     <div>
       <h2 className="viewprogram-title">Your Training Programs</h2>
       {trainingPrograms.length > 0 ? (
-        <ul>
-        {trainingPrograms.map((program) => (
-          <li key={program.id} className="program-column">
-            <h3 className="program-name-title">{program.title}</h3>
-            <ul className="program-day-list">
-              {program.trainingDays.map((trainingDay) => (
-                <li key={trainingDay.name}>
-                  <h4 className="trainingday-name">{trainingDay.name}</h4>
-                  <ul className="exercise-name-list">
-                    {trainingDay.exercises.map((exercise) => (
-                      <li className="content" key={exercise.name}>
-                        <p className="exercise-name">{exercise.name}</p>
-                        <p>Sets: {exercise.sets}</p>
-                        <p>Reps: {exercise.reps}</p>
-                      </li>
-                    ))}
-                  </ul>
-                </li>
+        <div className="program_table_div">
+            <h2>Click on a training program to view it</h2>
+        <div>
+            <table className="program_table">
+                
+                    <thead>
+                        <tr>
+                            <td className="tableHeader">
+                                <h3 className="HeaderText">
+                                    Programs
+                                </h3>
+                            </td>
+                            <td className="tableHeader">
+                                <h3 className="HeaderText">
+                                    Week:
+                                </h3>
+                            </td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {trainingPrograms.map((program) => (
+                            <tr>
+                                <button onClick={() => setUserPrograms(program)} className="training-submit">
+                                <td>
+                                    {program.title}
+                                </td>
+                                <td text-align="center">
+                                    1
+                                </td>
+                                </button>
+                            </tr>))}
+                    </tbody>
+            </table>
+      </div>
+
+      <br></br>
+      {userPrograms ? (
+
+        <div>
+        <h2 className="viewprogram-title">{userPrograms.title}:</h2>
+        <div className="Modified DayTable">
+            <table className="program_table">
+                <thead>
+                    <tr>
+                {userPrograms.trainingDays.map((trainingDay) => (
+                            <td className="tableHeader">
+                                <h3 className="HeaderText">
+                                    {trainingDay.name}
+                                </h3>
+                            </td>
+                ))}
+                </tr>
+                </thead>
+
+                <tbody>
+                <tr>    
+                {userPrograms.trainingDays.map((trainingDay) => (
+                    
+                        <td>
+                            <div className="Unmodified DayTable">
+                            <table className="program_table">
+                                <thead  className="SubtableHead">
+                                    <tr>
+                                        <td>
+                                            <h4 className="HeaderText">
+                                                Name
+                                            </h4>
+                                        </td>
+                                        <td>
+                                            <h4 className="HeaderText">
+                                                Sets
+                                            </h4>
+                                        </td>
+                                        <td>
+                                            <h4 className="HeaderText">
+                                                Reps
+                                            </h4>
+                                        </td>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                {trainingDay.exercises.map((exercise) => (
+                                        <tr>
+                                            <td>
+                                                {exercise.name}
+                                            </td>
+                                            <td>
+                                                {exercise.sets}
+                                            </td>
+                                            <td>
+                                                {exercise.reps}
+                                            </td>
+                                        </tr>
+                                ))}
+                                </tbody>  
+                            </table></div>
+                        </td>
+                    
               ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
+              </tr>
+              </tbody>
+
+            </table>
+            
+        </div>
+        <br></br>
+        </div>) : (<h2>No program has been selected yet</h2>)
+        }
+      </div>
+      
       ) : (
         <p>No training programs found.</p>
       )}
+
     </div>
   );
 }
